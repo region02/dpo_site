60aabb719722796652eaa6e77a2101333c08373f
bezura <64986427+bezura@users.noreply.github.com>
2024-02-05 22:06:39 +0300

    Короче, пофиксил, пересоздавай всю бд)))

--- database/migrations/2024_02_05_150040_create_courses_table.php
+++ database/migrations/2024_02_05_150040_create_courses_table.php
@@ -11,24 +11,47 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('courses', function (Blueprint $table) {
-//            $table->unsignedBigInteger('teacher_id')->nullable();
-//            $table->foreign('teacher_id')->references('id')->on('teachers');
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->string('subtitle');
+            $table->string('avatar');
+            $table->foreignId('course_type_id')->constrained('course_types')->cascadeOnDelete();
+            $table->boolean('started');
+
+            $table->foreignId('teacher_id')->nullable()->constrained('teachers');
             $table->string('slug');
+
             $table->date('start_at')->nullable();
             $table->string('duration');
             $table->string('load');
             $table->string('format');
+
             $table->json('section_who');
+
             $table->text('section_about_what');
+
             $table->json('section_result');
+
             $table->json('section_how_it_goes');
+
             $table->json('section_schedule');
+
+
+            $table->integer('cost');
             $table->text('cost_description');
 
-
+            $table->timestamps();
 
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
+//    public function down(): void
+//    {
+//        Schema::dropIfExists('courses');
+//    }
 };
