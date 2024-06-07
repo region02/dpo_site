e6deb5c0fb737f11cc1432e9930a0f72913d12d2
vkh <vl@d.d>
2024-06-07 17:36:29 +0300

    hotfix

--- app/MoonShine/Pages/Course/CourseFormPage.php
+++ app/MoonShine/Pages/Course/CourseFormPage.php
@@ -41,7 +41,7 @@ class CourseFormPage extends FormPage
                         Number::make('Стоимость', 'cost')
                             ->default(60000),
                         Image::make('Изображение', 'avatar')
-                            ->when(fn() => is_null($this->getItem()?->avatar), fn(Image $field) => $field->required())
+                            ->when(fn(Image $field) => is_null($field->getData()), fn(Image $field) => $field->required())
                             ->disk('public')
                             ->dir('course/avatar'),
                     ])
